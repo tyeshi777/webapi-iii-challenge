87ce5b4cb408815d23070b39494588ec8732430c
Yeshi <tyeshi777@yahoo.com>
2019-04-24 14:15:08 -0700

    post done

--- data/helpers/postDb-router.js
+++ data/helpers/postDb-router.js
@@ -14,4 +14,47 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const postId = req.params.id;
+  Post.getById(postId)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "cannot get by id" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newPost = req.body;
+  Post.insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "cannot insert" });
+    });
+});
+
+router.put("/", (req, res) => {
+  const postId = req.params.id;
+  const postBody = req.body;
+  Post.update(postId, postBody)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "no updating bro" });
+    });
+});
+router.delete("/:id", (req, res) => {
+  const postId = req.params.id;
+  Post.remove(postId)
+    .then(post => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      res.status(500).json({ err: "cannot delete" });
+    });
+});
 module.exports = router;
